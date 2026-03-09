8ea7a5b262a1ec8dc0d4655d61a7a004d60e3ecf
masud3239 <mdsikdermasud@gmail.com>
2026-03-09 20:08:46 +0600

    card border added and update

--- main.js
+++ main.js
@@ -41,10 +41,75 @@ issues.forEach(issue=>{
 
 const div=document.createElement("div");
 
+// div.innerHTML=`
+
+// <div onclick="openModal(${issue.id})"
+// class="card bg-base-100 shadow-md p-5 hover:shadow-lg transition">
+
+// <!-- Top Section -->
+
+// <div class="flex justify-between items-center mb-2">
+
+// <div class="flex items-center gap-2">
+
+// <div class="w-6 h-6 flex items-center justify-center">
+// <img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
+// </div>
+
+// </div>
+
+// <span class="px-3 py-1 text-xs font-semibold rounded-full
+// ${issue.priority==="high"?"bg-red-100 text-red-500":
+// issue.priority==="medium"?"bg-yellow-100 text-yellow-600":
+// "bg-gray-200 text-[#64748B]"}">
+
+// ${issue.priority.toUpperCase()}
+
+// </span>
+
+// </div>
+
+// <h2 class="font-bold text-lg leading-tight">
+
+// ${issue.title}
+
+// </h2>
+
+// <p class="text-sm text-[#64748B] mt-2">
+
+// ${issue.description.slice(0,80)}...
+
+// </p>
+
+// <div class="flex gap-2 mt-4">
+
+// <span class="px-3 py-1 text-xs rounded-full bg-red-100 text-red-500">
+// BUG
+// </span>
+
+// <span class="px-3 py-1 text-xs rounded-full bg-yellow-100 text-yellow-600">
+// ${issue.label ? issue.label : "HELP WANTED"}
+// </span>
+
+// </div>
+
+// <div class="text-sm text-gray-400 mt-4">
+
+// <p>#${issue.id} by ${issue.author}</p>
+
+// <p>${new Date(issue.createdAt).toLocaleDateString("en-US")}</p>
+
+// </div>
+
+// </div>
+
+// `;
+
 div.innerHTML=`
 
 <div onclick="openModal(${issue.id})"
-class="card bg-base-100 shadow-md p-5 hover:shadow-lg transition">
+class="card bg-base-100 shadow-md p-5 hover:shadow-lg transition border-t-4
+${issue.status==="open" ? "border-green-500" : "border-purple-500"}">
 
 <!-- Top Section -->
 
@@ -70,15 +135,11 @@ ${issue.priority.toUpperCase()}
 </div>
 
 <h2 class="font-bold text-lg leading-tight">
-
 ${issue.title}
-
 </h2>
 
 <p class="text-sm text-[#64748B] mt-2">
-
 ${issue.description.slice(0,80)}...
-
 </p>
 
 <div class="flex gap-2 mt-4">
