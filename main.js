d94f58a4ee9d0d71696bd8d187391189d3a18ca8
masud3239 <mdsikdermasud@gmail.com>
2026-03-09 17:28:26 +0600

    Card style fix

--- main.js
+++ main.js
@@ -44,42 +44,60 @@ const div=document.createElement("div");
 div.innerHTML=`
 
 <div onclick="openModal(${issue.id})"
-class="card bg-base-100 shadow border-t-4
-${issue.status==="open"?"border-green-500":"border-purple-500"}">
+class="card bg-base-100 shadow-md p-5 hover:shadow-lg transition">
 
-<div class="card-body">
+<!-- Top Section -->
 
-<h2 class="card-title">
-${issue.title}
-</h2>
+<div class="flex justify-between items-center mb-2">
 
-<p>
-${issue.description.slice(0,80)}...
-</p>
+<div class="flex items-center gap-2">
 
-<div class="flex gap-2">
+<div class="w-6 h-6 flex items-center justify-center">
+<img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
+</div>
 
-<span class="badge badge-success">
-${issue.status}
-</span>
+</div>
 
-<span class="badge badge-info">
-${issue.priority}
-</span>
+<span class="px-3 py-1 text-xs font-semibold rounded-full
+${issue.priority==="high"?"bg-red-100 text-red-500":
+issue.priority==="medium"?"bg-yellow-100 text-yellow-600":
+"bg-gray-200 text-[#64748B]"}">
 
-<span class="badge badge-warning">
-${issue.label}
+${issue.priority.toUpperCase()}
+
 </span>
 
 </div>
 
-<p class="text-sm text-[#64748B]">
-Author: ${issue.author}
+<h2 class="font-bold text-lg leading-tight">
+
+${issue.title}
+
+</h2>
+
+<p class="text-sm text-[#64748B] mt-2">
+
+${issue.description.slice(0,80)}...
+
 </p>
 
-<p class="text-xs text-[#64748B]">
-${issue.createdAt}
-</p>
+<div class="flex gap-2 mt-4">
+
+<span class="px-3 py-1 text-xs rounded-full bg-red-100 text-red-500">
+BUG
+</span>
+
+<span class="px-3 py-1 text-xs rounded-full bg-yellow-100 text-yellow-600">
+${issue.label ? issue.label : "HELP WANTED"}
+</span>
+
+</div>
+
+<div class="text-sm text-gray-400 mt-4">
+
+<p>#${issue.id} by ${issue.author}</p>
+
+<p>${new Date(issue.createdAt).toLocaleDateString("en-US")}</p>
 
 </div>
 
@@ -94,7 +112,7 @@ container.appendChild(div);
 }
 
 
-// TAB FILTER
+// Tab Filter
 
 function filterTab(type,btn){
 
